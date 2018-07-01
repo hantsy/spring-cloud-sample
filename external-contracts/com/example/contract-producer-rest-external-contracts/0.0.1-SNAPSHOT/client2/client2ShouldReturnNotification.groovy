8b5856180b22d93899c9a7411b80fe106d804844
hantsy <hantsy@gmail.com>
2018-07-01 08:50:11 +0800

    Added external contracts sample.

---
+++ external-contracts/com/example/contract-producer-rest-external-contracts/0.0.1-SNAPSHOT/client2/client2ShouldReturnNotification.groovy
@@ -0,0 +1,24 @@
+org.springframework.cloud.contract.spec.Contract.make {
+    description("""
+        Get notification by id
+    """)
+    // input to the contract
+    request {
+        url "/notifications/1"
+        method GET()
+        headers {
+            header("accept", applicationJson())
+        }
+    }
+    response {
+        status( 200)
+        body([
+            body: regex("test message.*"),
+            type: "MESSAGE"
+        ])
+        headers {
+            header("Content-Type", applicationJsonUtf8())
+        }
+    }
+}
+
